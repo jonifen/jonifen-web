f41f346cbc66d03632e9f9da486c114ea4a914c2
Jon Cain <jonifen@users.noreply.github.com>
2020-01-16 20:49:52 +0000

    Add minify parameter to hugo in deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo --minify # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 cd public
 git add .
